75b4b0a3c78eed460a5c1a9db99271051d20c1a5
hk9981 <hk9981@srmist.edu.in>
2026-02-14 22:23:10 +0530

    UC2: Add UC2PrintBanner.java to print OOPS in banner format

---
+++ UC2PrintBanner.java
@@ -0,0 +1,18 @@
+/**
+ * UC2: Print OOPS in Banner Format
+ *
+ * @author hk9981
+ * @version 1.0
+ */
+public class UC2PrintBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println("  ***   " + "  ***   " + " ****   " + " ***** ");
+        System.out.println(" *   *  " + " *   *  " + " *   *  " + " *     ");
+        System.out.println("*     * " + "*     * " + " ****   " + " ****  ");
+        System.out.println("*     * " + "*     * " + " *      " + "     * ");
+        System.out.println(" *   *  " + " *   *  " + " *      " + " *   * ");
+        System.out.println("  ***   " + "  ***   " + " *      " + " ***** ");
+    }
+}
